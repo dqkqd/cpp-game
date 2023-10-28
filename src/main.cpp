5d84c876091fd1b46dc32b2fd74e7f4219a3b731
Khanh Duong Quoc <dqkqdlot@gmail.com>
2023-10-28 20:06:57 +0900

    feat: chapter 5

--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,30 @@
 #include "SDL_error.h"
+#include "SDL_events.h"
 #include "SDL_init.h"
+#include "SDL_keycode.h"
 #include "SDL_log.h"
 #include "SDL_oldnames.h"
 #include "SDL_pixels.h"
 #include "SDL_surface.h"
 #include "SDL_video.h"
 #include <SDL3/SDL.h>
+#include <string>
+
+enum class KeyPressSurfaces {
+  KEY_PRESS_SURFACE_DEFAULT,
+  KEY_PRESS_SURFACE_UP,
+  KEY_PRESS_SURFACE_DOWN,
+  KEY_PRESS_SURFACE_LEFT,
+  KEY_PRESS_SURFACE_RIGHT,
+  KEY_PRESS_SURFACE_TOTAL
+};
 
 SDL_Window *window = NULL;
 SDL_Surface *screenSurface = NULL;
 SDL_Surface *image = NULL;
+SDL_Surface *keyPressSurfaces[static_cast<size_t>(
+    KeyPressSurfaces::KEY_PRESS_SURFACE_TOTAL)];
+SDL_Surface *currentSurface = NULL;
 
 bool init() {
 
@@ -26,10 +41,6 @@ bool init() {
       success = false;
     } else {
       screenSurface = SDL_GetWindowSurface(window);
-      // SDL_FillSurfaceRect(screenSurface, nullptr,
-      //                     SDL_MapRGB(screenSurface->format, 0xFF, 0xFF,
-      //                     0xFF));
-      // SDL_UpdateWindowSurface(window);
     }
   }
 
@@ -43,29 +54,105 @@ void close() {
   SDL_Quit();
 }
 
+SDL_Surface *loadSurface(std::string path) {
+  auto loadedSurface = SDL_LoadBMP(path.c_str());
+  if (!loadedSurface) {
+    SDL_Log("%s %s", path.c_str(), SDL_GetError());
+    return nullptr;
+  }
+
+  auto optimizedSurface =
+      SDL_ConvertSurface(loadedSurface, loadedSurface->format);
+  SDL_DestroySurface(loadedSurface);
+
+  return optimizedSurface;
+}
+
 bool loadMedia() {
   bool success = true;
-  image = SDL_LoadBMP("assets/hello_world.bmp");
-  if (!image) {
-    SDL_Log("%s", SDL_GetError());
+
+  // default
+  keyPressSurfaces[int(KeyPressSurfaces::KEY_PRESS_SURFACE_DEFAULT)] =
+      loadSurface("assets/press.bmp");
+  if (!keyPressSurfaces[int(KeyPressSurfaces::KEY_PRESS_SURFACE_DEFAULT)]) {
     success = false;
   }
+
+  // up
+  keyPressSurfaces[int(KeyPressSurfaces::KEY_PRESS_SURFACE_UP)] =
+      loadSurface("assets/up.bmp");
+  if (!keyPressSurfaces[int(KeyPressSurfaces::KEY_PRESS_SURFACE_UP)]) {
+    success = false;
+  }
+
+  // down
+  keyPressSurfaces[int(KeyPressSurfaces::KEY_PRESS_SURFACE_DOWN)] =
+      loadSurface("assets/down.bmp");
+  if (!keyPressSurfaces[int(KeyPressSurfaces::KEY_PRESS_SURFACE_DOWN)]) {
+    success = false;
+  }
+
+  // left
+  keyPressSurfaces[int(KeyPressSurfaces::KEY_PRESS_SURFACE_LEFT)] =
+      loadSurface("assets/left.bmp");
+  if (!keyPressSurfaces[int(KeyPressSurfaces::KEY_PRESS_SURFACE_LEFT)]) {
+    success = false;
+  }
+
+  // right
+  keyPressSurfaces[int(KeyPressSurfaces::KEY_PRESS_SURFACE_RIGHT)] =
+      loadSurface("assets/right.bmp");
+  if (!keyPressSurfaces[int(KeyPressSurfaces::KEY_PRESS_SURFACE_RIGHT)]) {
+    success = false;
+  }
+
   return success;
 }
 
 void gameLoop() {
-  SDL_BlitSurface(image, nullptr, screenSurface, nullptr);
-  SDL_UpdateWindowSurface(window);
+
+  SDL_Event event;
+  currentSurface =
+      keyPressSurfaces[int(KeyPressSurfaces::KEY_PRESS_SURFACE_DEFAULT)];
 
   bool quit = false;
   while (!quit) {
-    SDL_Event event;
     while (SDL_PollEvent(&event)) {
       if (event.type == SDL_EVENT_QUIT) {
         quit = true;
         break;
       }
+
+      // user presses a key
+      else if (event.type == SDL_EVENT_KEY_DOWN) {
+        switch (event.key.keysym.sym) {
+        case SDLK_UP:
+          currentSurface =
+              keyPressSurfaces[int(KeyPressSurfaces::KEY_PRESS_SURFACE_UP)];
+          break;
+        case SDLK_DOWN:
+          currentSurface =
+              keyPressSurfaces[int(KeyPressSurfaces::KEY_PRESS_SURFACE_DOWN)];
+          break;
+        case SDLK_LEFT:
+          currentSurface =
+              keyPressSurfaces[int(KeyPressSurfaces::KEY_PRESS_SURFACE_LEFT)];
+          break;
+        case SDLK_RIGHT:
+          currentSurface =
+              keyPressSurfaces[int(KeyPressSurfaces::KEY_PRESS_SURFACE_RIGHT)];
+          break;
+        default:
+          currentSurface = keyPressSurfaces[int(
+              KeyPressSurfaces::KEY_PRESS_SURFACE_DEFAULT)];
+          break;
+        }
+      }
+
+      SDL_BlitSurface(currentSurface, nullptr, screenSurface, nullptr);
+      SDL_UpdateWindowSurface(window);
     }
+
     if (quit) {
       break;
     }
