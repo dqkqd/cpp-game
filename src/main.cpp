2797de51022e43a6caf8564cdc6f33aa30607269
Khanh Duong Quoc <dqkqdlot@gmail.com>
2023-10-29 12:14:24 +0900

    feat: chapter 14

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,6 @@
 #include "SDL_blendmode.h"
+#include "SDL_error.h"
+#include "SDL_log.h"
 #include "SDL_render.h"
 #include <SDL3/SDL.h>
 #include <SDL3_image/SDL_image.h>
@@ -73,8 +75,9 @@ private:
 SDL_Surface *screenSurface = NULL;
 SDL_Surface *currentSurface = NULL;
 
-LTexture modulatedTexture;
-LTexture backgroundTexture;
+const int WALKING_ANIMATION_FRAMES = 4;
+SDL_FRect spriteCLips[WALKING_ANIMATION_FRAMES];
+LTexture spriteTexture;
 
 constexpr int SCREEN_WIDTH = 640;
 constexpr int SCREEN_HEIGHT = 480;
@@ -95,7 +98,8 @@ bool init() {
       success = false;
     } else {
 
-      renderer = SDL_CreateRenderer(window, NULL, SDL_RENDERER_ACCELERATED);
+      renderer = SDL_CreateRenderer(
+          window, NULL, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
       if (!renderer) {
         SDL_Log("%s", SDL_GetError());
         success = false;
@@ -124,14 +128,30 @@ void close() {
 bool loadMedia() {
   bool success = true;
 
-  if (!modulatedTexture.loadFromFile("assets/13_alpha_blending/fadeout.png")) {
+  if (!spriteTexture.loadFromFile(
+          "assets/14_animated_sprites_and_vsync/foo.png")) {
+    SDL_Log("%s", SDL_GetError());
     success = false;
   } else {
-    modulatedTexture.setBlendMode(SDL_BLENDMODE_BLEND);
-  }
+    spriteCLips[0].x = 0;
+    spriteCLips[0].y = 0;
+    spriteCLips[0].w = 64;
+    spriteCLips[0].h = 205;
 
-  if (!backgroundTexture.loadFromFile("assets/13_alpha_blending/fadein.png")) {
-    success = false;
+    spriteCLips[1].x = 64;
+    spriteCLips[1].y = 0;
+    spriteCLips[1].w = 64;
+    spriteCLips[1].h = 205;
+
+    spriteCLips[2].x = 128;
+    spriteCLips[2].y = 0;
+    spriteCLips[2].w = 64;
+    spriteCLips[2].h = 205;
+
+    spriteCLips[3].x = 192;
+    spriteCLips[3].y = 0;
+    spriteCLips[3].w = 64;
+    spriteCLips[3].h = 205;
   }
 
   return success;
@@ -142,31 +162,27 @@ void gameLoop() {
   SDL_Event event;
 
   bool quit = false;
-  uint8_t a = 255;
+  int frame = 0;
 
   while (!quit) {
-    while (SDL_PollEvent(&event)) {
+    while (SDL_PollEvent(&event) != 0) {
       if (event.type == SDL_EVENT_QUIT) {
         quit = true;
         break;
-      } else if (event.type == SDL_EVENT_KEY_DOWN) {
-        switch (event.key.keysym.sym) {
-        case SDLK_w:
-          a = std::min(255, a + 32);
-          break;
-        case SDLK_s:
-          a = std::max(0, a - 32);
-          break;
-        }
       }
+    }
 
-      SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
-      SDL_RenderClear(renderer);
-      backgroundTexture.render(0, 0);
+    SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
+    SDL_RenderClear(renderer);
 
-      modulatedTexture.setAlpha(a);
-      modulatedTexture.render(0, 0);
-      SDL_RenderPresent(renderer);
+    auto currentClip = &spriteCLips[frame / 4];
+    spriteTexture.render((SCREEN_WIDTH - currentClip->w) / 2,
+                         (SCREEN_HEIGHT - currentClip->h) / 2, currentClip);
+    SDL_RenderPresent(renderer);
+
+    ++frame;
+    if (frame / 4 >= WALKING_ANIMATION_FRAMES) {
+      frame = 0;
     }
 
     if (quit) {
