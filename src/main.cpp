7b1ead12f94b421be3eb2641c38e672efbcb8511
Khanh Duong Quoc <dqkqdlot@gmail.com>
2023-10-29 12:45:27 +0900

    feat: chapter 15

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,15 @@
-#include "SDL_blendmode.h"
-#include "SDL_error.h"
-#include "SDL_log.h"
-#include "SDL_render.h"
 #include <SDL3/SDL.h>
 #include <SDL3_image/SDL_image.h>
+
 #include <cstddef>
 #include <cstdint>
 #include <string>
+
+#include "SDL_blendmode.h"
+#include "SDL_error.h"
+#include "SDL_events.h"
+#include "SDL_log.h"
+#include "SDL_render.h"
 
 enum class KeyPressSurfaces {
   KEY_PRESS_SURFACE_DEFAULT,
@@ -17,11 +20,11 @@ enum class KeyPressSurfaces {
   KEY_PRESS_SURFACE_TOTAL
 };
 
-SDL_Window *window = NULL;
-SDL_Renderer *renderer = NULL;
+SDL_Window* window = NULL;
+SDL_Renderer* renderer = NULL;
 
 class LTexture {
-public:
+ public:
   LTexture() {}
   ~LTexture() { free(); }
 
@@ -57,47 +60,47 @@ public:
     return true;
   }
 
-  void render(int x, int y, SDL_FRect *clip = NULL) {
+  int getWidth() { return width_; }
+  int getHeight() { return height_; }
+
+  void render(int x, int y, SDL_FRect* clip = NULL, double angle = 0.0,
+              SDL_FPoint* center = NULL,
+              SDL_RendererFlip flip = SDL_FLIP_NONE) {
     SDL_FRect viewport{float(x), float(y), float(width_), float(height_)};
     if (clip != NULL) {
       viewport.w = clip->w;
       viewport.h = clip->h;
     }
-    SDL_RenderTexture(renderer, texture_, clip, &viewport);
+    SDL_RenderTextureRotated(renderer, texture_, clip, &viewport, angle, center,
+                             flip);
   }
 
-private:
-  SDL_Texture *texture_;
+ private:
+  SDL_Texture* texture_;
   int width_;
   int height_;
 };
 
-SDL_Surface *screenSurface = NULL;
-SDL_Surface *currentSurface = NULL;
-
-const int WALKING_ANIMATION_FRAMES = 4;
-SDL_FRect spriteCLips[WALKING_ANIMATION_FRAMES];
-LTexture spriteTexture;
+SDL_Surface* screenSurface = NULL;
 
 constexpr int SCREEN_WIDTH = 640;
 constexpr int SCREEN_HEIGHT = 480;
 
-bool init() {
+LTexture currentTexture;
 
+bool init() {
   bool success = true;
 
   if (SDL_Init(SDL_INIT_VIDEO) < 0) {
     SDL_Log("SDL_Init failed (%s)", SDL_GetError());
     success = false;
   } else {
-
     window = SDL_CreateWindow("Hello SDL", SCREEN_WIDTH, SCREEN_HEIGHT,
                               SDL_WINDOW_OPENGL);
     if (!window) {
       SDL_Log("%s", SDL_GetError());
       success = false;
     } else {
-
       renderer = SDL_CreateRenderer(
           window, NULL, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
       if (!renderer) {
@@ -128,62 +131,55 @@ void close() {
 bool loadMedia() {
   bool success = true;
 
-  if (!spriteTexture.loadFromFile(
-          "assets/14_animated_sprites_and_vsync/foo.png")) {
+  if (!currentTexture.loadFromFile(
+          "assets/15_rotation_and_flipping/arrow.png")) {
     SDL_Log("%s", SDL_GetError());
     success = false;
-  } else {
-    spriteCLips[0].x = 0;
-    spriteCLips[0].y = 0;
-    spriteCLips[0].w = 64;
-    spriteCLips[0].h = 205;
-
-    spriteCLips[1].x = 64;
-    spriteCLips[1].y = 0;
-    spriteCLips[1].w = 64;
-    spriteCLips[1].h = 205;
-
-    spriteCLips[2].x = 128;
-    spriteCLips[2].y = 0;
-    spriteCLips[2].w = 64;
-    spriteCLips[2].h = 205;
-
-    spriteCLips[3].x = 192;
-    spriteCLips[3].y = 0;
-    spriteCLips[3].w = 64;
-    spriteCLips[3].h = 205;
   }
 
   return success;
 }
 
 void gameLoop() {
-
   SDL_Event event;
 
   bool quit = false;
-  int frame = 0;
+  double degrees = 0;
+  SDL_RendererFlip flipType = SDL_FLIP_NONE;
 
   while (!quit) {
     while (SDL_PollEvent(&event) != 0) {
       if (event.type == SDL_EVENT_QUIT) {
         quit = true;
         break;
+      } else if (event.type == SDL_EVENT_KEY_DOWN) {
+        switch (event.key.keysym.sym) {
+          case SDLK_a:
+            degrees -= 60;
+            break;
+          case SDLK_d:
+            degrees += 60;
+            break;
+          case SDLK_q:
+            flipType = SDL_FLIP_HORIZONTAL;
+            break;
+          case SDLK_w:
+            flipType = SDL_FLIP_NONE;
+            break;
+          case SDLK_e:
+            flipType = SDL_FLIP_VERTICAL;
+            break;
+        }
       }
     }
 
     SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
     SDL_RenderClear(renderer);
 
-    auto currentClip = &spriteCLips[frame / 4];
-    spriteTexture.render((SCREEN_WIDTH - currentClip->w) / 2,
-                         (SCREEN_HEIGHT - currentClip->h) / 2, currentClip);
+    currentTexture.render((SCREEN_WIDTH - currentTexture.getWidth()) / 2,
+                          (SCREEN_HEIGHT - currentTexture.getHeight()) / 2,
+                          NULL, degrees, NULL, flipType);
     SDL_RenderPresent(renderer);
-
-    ++frame;
-    if (frame / 4 >= WALKING_ANIMATION_FRAMES) {
-      frame = 0;
-    }
 
     if (quit) {
       break;
@@ -191,7 +187,7 @@ void gameLoop() {
   }
 }
 
-int main(int argc, char *argv[]) {
+int main(int argc, char* argv[]) {
   init();
   if (loadMedia()) {
     gameLoop();
